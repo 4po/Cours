b6b81268e78af6ef0fd172762fba8242cfde8c58
0xE <49166384+0x307845@users.noreply.github.com>
2020-09-26 16:25:59 +0200

    Add files via upload

---
+++ Secondes/Python/Introduction.py
@@ -0,0 +1,58 @@
+#==============================================================#
+
+                        #Type de variable
+
+a = 12 ---> integer(int)
+b = "Salut" ---> String(str)
+c = [12,1,7,"salut","toto"] ---> liste
+
+#==============================================================#
+
+                        #Instruction de base
+
+print("salut")
+-> salut
+
+print(12)
+-> 12
+
+x = 12
+print(x)
+-> 12
+
+#==============================================================#
+
+                            #Opérateur
+
+x = 10 + 2
+x = 10 - 2
+x = 10 * 2
+x = 10 // 2 #Quotient d'une division posé
+x = 10 % 2  #Reste d'une division posé
+x = 10 / 2  #Quotient d'un division normal
+
+#==============================================================#
+
+                    #Réatribution de varriable
+
+x = 12
+x = 'Salut'
+print(x)
+#Console : Salut
+
+#==============================================================#
+
+                        #Bloc conditiennel
+
+sexe = 'fille' #Je crée une varribale
+
+if sexe == 'fille' : #je teste si la varriable vaut 'fille'
+    print("F") #Affiche F si la var vaut Fille
+
+elif sexe == 'male' : #je teste si la varriable vaut 'male'
+    print("M") #Affiche M si la var vaut Male
+
+else : #Sinon ...
+    print("Autre chose que Fille ou garçon !") #affiche "Autre chose que Fille ou garçon !" si la var ne vaut ni fille ni male
+
+#==============================================================#
