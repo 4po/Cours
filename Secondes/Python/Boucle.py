228beec0539efc96192606eca0d68e7c3e7869b4
0xE <49166384+0x307845@users.noreply.github.com>
2020-09-26 16:26:54 +0200

    Add files via upload

---
+++ Secondes/Python/Boucle.py
@@ -0,0 +1,47 @@
+#==============================================================#
+
+                            # Boucle For :
+
+for [Iterateur] in [Object] :
+    Instruction 1
+    Instruction 2
+    ...
+    Instruction n
+
+#Exemple :
+
+for i in range(3) :
+    print(i)
+#Console : 0
+#          1
+#          2
+
+#==============================================================#
+
+                            #Boucle While :
+
+while [condition] :
+    Instruction 1
+    Instruction 2
+    ...
+    Instruction n
+
+#Exemple :
+
+a = True
+while a == True :
+    compteur = 0
+    compteur = compteur + 1
+    if compteur == 2
+        a = False
+    print(compteur)
+#Console : 0
+#          1
+
+compteur 0
+while compteur < 2 :
+    print(compteur)
+#Console : 0
+#          1
+
+#==============================================================#
